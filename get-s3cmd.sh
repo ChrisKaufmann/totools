525e025d564380870564e2c76824aac6ba3f1004
Chris Kaufmann <github@chriskaufmann.com>
2012-07-17 15:41:58 -0500

    New url for s3cmd

--- get-s3cmd.sh
+++ get-s3cmd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pushd /etc/yum.repos.d
-	wget http://s3tools.org/repo/Centos_$releasever/s3tools.repo
+	wget http://s3tools.org/repo/RHEL_6/s3tools.repo
 	yum -y install s3cmd
 popd
