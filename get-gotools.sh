b84dcbbff4e9bab5fbd028a1694a79cead34b44d
Chris Kaufmann <github@chriskaufmann.com>
2013-02-01 16:56:08 -0600

    Small changes to move from go to to

--- get-gotools.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-location='https://raw.github.com/ChrisKaufmann/gotools/master/'
-for thing in ami background didi ducks go gox killthe killport
-do
-	curl $location/$thing -o /usr/local/bin/$thing
-	chmod +x /usr/local/bin/$thing
-done
---
+++ get-totools.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+location='https://raw.github.com/ChrisKaufmann/totools/master/'
+for thing in ami background didi ducks to tox killthe killport
+do
+	curl $location/$thing -o /usr/local/bin/$thing
+	chmod +x /usr/local/bin/$thing
+done
