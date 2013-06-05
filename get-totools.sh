e3219289f989c6d6b04e78ddd032d418b823445e
Chris Kaufmann <ckaufmann@connamara.com>
2013-06-05 15:59:07 -0500

    Added the "loop" command

--- get-totools.sh
+++ get-totools.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 location='https://raw.github.com/ChrisKaufmann/totools/master/'
-for thing in ami amp background didi ducks to tox killthe killport pwgen
+for thing in ami amp background didi ducks to tox killthe killport pwgen loop
 do
 	curl $location/$thing -o /usr/local/bin/$thing
 	chmod +x /usr/local/bin/$thing
