cb2217cba171463a456008a1707c6c46f5fe1e1d
Chris Kaufmann <ckaufmann@connamara.com>
2014-02-11 15:52:06 -0600

    Added spork, updated loop to take a file, and updated the getter

--- get-totools.sh
+++ get-totools.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 location='https://raw.github.com/ChrisKaufmann/totools/master/'
-for thing in ami amp background didi ducks to tox killthe killport pwgen loop radduser
+for thing in ami amp background didi ducks to tox killthe killport pwgen loop radduser spork
 do
 	curl $location/$thing -o /usr/local/bin/$thing
 	chmod +x /usr/local/bin/$thing
