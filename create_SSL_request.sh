3c4a3398571fb9b8eecb362418e8dda3d004e016
Chris Kaufmann <ckaufmann@connamara.com>
2014-08-05 16:18:37 -0500

    minor updates :)

--- create_SSL_request.sh
+++ create_SSL_request.sh
@@ -67,6 +67,7 @@ if [[ ! -e $DESTDIR ]]
 		mkdir -p $DESTDIR
 fi
 pushd $DESTDIR
+echo $PASS >> passphrase
 openssl genrsa -aes256 -passout pass:$PASS -out ${DN}.encrypt.key 2048
 openssl rsa -in ${DN}.encrypt.key -passin pass:$PASS -out ${DN}.key
 openssl req -new -key ${DN}.key -out ${DN}.csr -subj "/CN=${DN}/emailAddress=$EMAIL/O=$ORG/C=$COUNTRY/ST=$STATE/L=$LOC"
