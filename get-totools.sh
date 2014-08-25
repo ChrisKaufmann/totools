9dd0a0d9d8531fe2c5f1b5155846b52e8b8b6037
Chris Kaufmann <github@chriskaufmann.com>
2014-08-25 16:00:21 -0500

    Changed threading in spork, and updated the getter

--- get-totools.sh
+++ get-totools.sh
@@ -5,3 +5,4 @@ do
 	curl $location/$thing -o /usr/local/bin/$thing
 	chmod +x /usr/local/bin/$thing
 done
+curl https://raw.githubusercontent.com/ChrisKaufmann/Threadeach/master/threadeach.pm -o /usr/local/lib64/perl5/
